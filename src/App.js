525d1cf302a609ae06d87b834e295045e04461c7
Angad Kambli <angadkambli@gmail.com>
2021-12-03 16:02:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useRef, useState } from "react";
+import Chess from "./game/scene/main_scene";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div style={{ position: "relative"}}>
+			<Chess></Chess>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/game/scene/main_scene.js
@@ -0,0 +1,69 @@
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame, useThree, extend } from "@react-three/fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+extend({ OrbitControls });
+
+function Box(props) {
+	// This reference will give us direct access to the mesh
+	const mesh = useRef();
+	// Set up state for the hovered and active state
+	const [hovered, setHover] = useState(false);
+	const [active, setActive] = useState(false);
+	// Subscribe this component to the render-loop, rotate the mesh every frame
+	//useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
+	// Return view, these are regular three.js elements expressed in JSX
+	return (
+		<mesh
+			position={props.position}
+			ref={mesh}
+			scale={active ? 1.5 : 1}
+			onClick={(event) => setActive(!active)}
+			onPointerOver={(event) => setHover(true)}
+			onPointerOut={(event) => setHover(false)}
+		>
+			<boxGeometry args={[1, 1, 1]} />
+			<meshStandardMaterial
+				color={
+					hovered
+						? "grey"
+						: (props.row + props.col) % 2 == 0
+						? "black"
+						: "white"
+				}
+				pixelRatio={2.0}
+			/>
+		</mesh>
+	);
+}
+
+const Board = () => {};
+
+const CameraControls = () => {
+	const {
+		camera,
+		gl: { domElement },
+	} = useThree();
+	const controls = useRef();
+	useFrame((state) => controls.current.update());
+	return <orbitControls ref={controls} args={[camera, domElement]} />;
+};
+
+const Chess = () => {
+	return (
+		<Canvas pixelRatio={window.devicePixelRatio}>
+			<CameraControls />
+			<ambientLight />
+			<pointLight position={[10, 10, 10]} />
+			{[...Array(8)].map((x, i) => {
+				return [...Array(8)].map((y, j) => {
+					return (
+						<Box position={[i - 4.5, j - 4.5, 0]} row={i} col={j} />
+					);
+				});
+			})}
+		</Canvas>
+	);
+};
+
+export default Chess;
