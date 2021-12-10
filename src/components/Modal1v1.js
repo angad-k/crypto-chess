d026039ca6ac4b16fa531add635fec67ad06e661
saurabh.s3 <saurabh.s@fampay.in>
2021-12-11 01:01:20 +0530

    added modals

--- src/components/Modal1v1.js
+++ src/components/Modal1v1.js
@@ -1,3 +1,11 @@
+import {
+  modalStyles,
+  modalHeaderStyles,
+  modalInputStyles,
+  modalPrimaryCtaStyles,
+  modalSecCtaStyles,
+} from "../utils/theme";
+
 const Modal1v1 = ({ type, closeModal }) => {
   const handleJoin = () => {};
   const handleCancel = () => closeModal();
@@ -5,16 +13,21 @@ const Modal1v1 = ({ type, closeModal }) => {
 
   return (
     <div className="bg-opacity-30 bg-black fixed top-0 h-screen w-screen flex items-center justify-center backdrop-filter backdrop-blur-md">
-      <div>
-        <p>{type} Game</p>
+      <div className={modalStyles}>
+        <p className={modalHeaderStyles}>{type} Game</p>
         <input
           type="text"
           placeholder={
-            type === "Join" ? "Enter Game code" : "Enter Coins to stake"
+            type === "Join" ? "Enter Game Code" : "Enter Coins to Stake"
           }
+          className={modalInputStyles}
         />
-        {type === "Join" ? <button>Join</button> : <button>Start</button>}
-        <button onClick={handleCancel}>Cancel</button>
+        <button className={modalPrimaryCtaStyles}>
+          {type === "Join" ? "Join" : "Create"}
+        </button>
+        <button onClick={handleCancel} className={`${modalSecCtaStyles} ml-2`}>
+          Cancel
+        </button>
       </div>
     </div>
   );
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,2 +1,15 @@
 export const cardCtaStyles =
   "bg-b2 py-1 flex-grow rounded-full text-sm font-medium text-white text-center border boder-white hover:bg-white hover:text-b2 transition";
+
+  export const modalStyles = "bg-white max-w-sm rounded-lg p-6";
+  export const modalHeaderStyles =
+    "text-2xl font-bold text-center text-b2 mb-4";
+
+  export const modalInputStyles =
+    "outline-none bg-b2 bg-opacity-10 rounded px-4 py-2 w-full mb-6";
+
+  export const modalPrimaryCtaStyles =
+    "bg-b2 rounded-full px-8 py-2 text-white text-center font-medium";
+
+  export const modalSecCtaStyles =
+    "bg-white rounded-full px-8 py-2 text-b2 text-center font-medium border border-b2";
