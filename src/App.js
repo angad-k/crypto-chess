ade3143978262b05488a5b74c4e170074325517b
mayankmittal-iitr <mmittal123456@gmail.com>
2021-12-10 17:57:44 +0530

    updated smart contracts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Suspense, useEffect, useState } from "react";
 import getWeb3 from "./utils/getWeb3";
-import Chess from "../build.eth/contracts/Chess.json";
+import Chess from "./build.eth/contracts/Chess.json";
 
 const App = () => {
   const [Web3, setWeb3] = useState(null);
@@ -14,7 +14,7 @@ const App = () => {
       console.log(accounts[0]);
       const networkId = await web3.eth.net.getId();
       const deployedNetwork = Chess.networks[networkId];
-      console.log(deployedNetwork);
+      console.log(deployedNetwork.address);
       const instance = new web3.eth.Contract(
         Chess.abi,
         deployedNetwork && deployedNetwork.address
@@ -36,7 +36,7 @@ const App = () => {
   return (
     <div style={{ height: "100vh", width: "100%" }}>
       <Suspense fallback={<></>}>
-        <Chess gameCode="abc" pubKey="xyz" isHost="true"></Chess>
+        {/* <Chess gameCode="abc" pubKey="xyz" isHost="true"></Chess> */}
       </Suspense>
     </div>
   );
