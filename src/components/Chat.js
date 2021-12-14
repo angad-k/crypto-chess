3908b3abbe58bcaac4ca74be2e5b283ce02c9393
Angad Kambli <angadkambli@gmail.com>
2021-12-14 17:00:22 +0530

    TopNav repeat bug fix

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -69,9 +69,9 @@ const Chat = (props) => {
 					onClick={() => {
 						setOpened(true);
 					}}
-					className="fixed bottom-10 right-10 bg-b2 w-400 p-3 text-white rounded-xl flex-row flex cursor-pointer justify-center"
+					className="fixed bottom-10 right-10 bg-b2  p-3 text-white rounded-full flex-row flex cursor-pointer justify-center"
 				>
-					<p className="mr-4 text-sm font-medium">Chat</p>
+					<p className=" text-sm font-medium">Chat</p>
 				</div>
 			</>
 		);
@@ -106,7 +106,7 @@ const Chat = (props) => {
 							className="bg-b1 mt-0.5 appearance-none text-white rounded-xl py-2 px-4 opacity-80 leading-tight focus:outline-none w-4/5"
 						/>
 						<span className="p-1 pl-2.5 pr-2.5 mt-0.5 bg-primary text-white rounded cursor-pointer">
-							{">>"}
+							{">"}
 						</span>
 					</div>
 				</div>
